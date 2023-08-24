93ea0734fb03d90fa6edb9f55b19e2d19b2ab321
waimate <waimate@nhht.co.nz>
2023-08-24 16:18:02 +1200

    added filter search & tags

--- modules/clients.py
+++ modules/clients.py
@@ -13,7 +13,7 @@ class Clients(Base):
         else:
             raise Exception("Could not get client information," + response.text)
     
-    def search_clients(self, query:str, size:int) -> dict:
+    def search_clients(self, query:str, size=10) -> dict:
         """Search for clients"""
         args = {
             'activeonly': 'true', # Only return active clients
@@ -23,6 +23,32 @@ class Clients(Base):
         
         url = 'clients/search?' + '&'.join([f'{key}={value}' for key, value in args.items()])
         response = self.get(url)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception("Could not search for clients," + response.text)
+    
+    def get_client_tags(self, client_id:str) -> dict:
+        """Get tags for a client"""
+        response = self.get(f'client-tags/client/{client_id}')
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception("Could not get client tags," + response.text)
+    
+    def search_with_filter(self, filters,
+        page=0,
+        options={
+            "inactiveClients": False,
+            "size": 15,
+            "sortattr": "dateOfBirth",
+            "sortdir": "ASC",
+            "timeZone": "Pacific/Auckland"
+        }):
+        """Search for clients with filters"""
+        url = 'clients-search'
+        response = self.post(url, json={"filters": filters, "options": {**options, "page": page}})
 
         if response.status_code == 200:
             return response.json()
